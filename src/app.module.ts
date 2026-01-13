c9c2d806f832f1e4bbd751f7f9f8367097a7916c
aifia105 <mohamedaifia34@gmail.com>
2026-01-13 15:04:18 +0100

    database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,49 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { OrganizationsModule } from './organizations/organizations.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      envFilePath: ['.env.development', '.env.production', '.env.staging'],
+    }),
+    // postgresDB
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.database'),
+        entities: [__dirname + '/**/*.schema{.ts,.js}'],
+        synchronize: false,
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
+    // mongoDB
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('mongoUri'),
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    OrganizationsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,53 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Task & Operations Platform')
+    .setDescription('The operations platform API description')
+    .setVersion('1.0')
+    .addTag('Task & Operations')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  app.use(cookieParser());
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(
+    '--------------------------------------------------------------------------------------',
+  );
+  console.log(
+    `Server running on port ${process.env.PORT} and you can access the API at ${process.env.BACKEND_URL}:${process.env.PORT}`,
+  );
+  console.log(
+    '--------------------------------------------------------------------------------------',
+  );
+  console.log(
+    `Swagger documentation available at ${process.env.BACKEND_URL}:${process.env.PORT}/api`,
+  );
+  console.log(
+    '--------------------------------------------------------------------------------------',
+  );
 }
 bootstrap();
